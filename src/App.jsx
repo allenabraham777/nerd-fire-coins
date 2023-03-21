9d61fbdda3d43fd54534e3514aea4ebbb9f2ad9e
Allen K Abraham <allenkabraham777@gmail.com>
2023-03-21 12:56:31 +0530

    Apply layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,53 @@
-import { useRef } from "react";
+import { useState } from "react";
 import "./App.css";
 import { airdrop } from "./assets/utils/solana";
 
 const App = () => {
-  const inputRef = useRef(null);
+  const [message, setMessage] = useState("");
+  const [walletAddress, setwalletAddress] = useState("");
 
   const airdropSolana = async () => {
-    const walletAddress = inputRef?.current?.value;
     if (!walletAddress) return;
+    setMessage("");
     try {
       const transactionId = await airdrop(walletAddress);
-      alert("Transaction: " + transactionId);
+      setMessage(
+        `Transaction successful, transaction id: ${transactionId}, <a href="https://explorer.solana.com/tx/${transactionId}?cluster=devnet" target="_blank">click here</a> for more details`
+      );
+      setwalletAddress("");
     } catch (error) {
       console.error(error);
+      setMessage(
+        `<span class="error">Error while airdropping, please try again later.${
+          error.message ? ` Error: ${error.message}` : ""
+        }</span>`
+      );
     }
   };
 
   return (
-    <div>
-      <h1>Nerd Fire Coins</h1>
-      <input type="text" ref={inputRef} />
-      <button onClick={airdropSolana}>Airdrop</button>
+    <div className="container">
+      <h1 className="container__heading">Solana Devnet Faucet</h1>
+      <div className="container__wrapper">
+        <input
+          className="container__input"
+          type="text"
+          value={walletAddress}
+          placeholder="Enter the wallet address"
+          onChange={(e) => setwalletAddress(e.target.value)}
+        />
+        <button
+          className="container__button"
+          onClick={airdropSolana}
+          disabled={!walletAddress}
+        >
+          Airdrop
+        </button>
+      </div>
+      <div
+        className="container__message"
+        dangerouslySetInnerHTML={{ __html: message }}
+      />
     </div>
   );
 };
